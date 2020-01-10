fe4af8cddc6d57de83b4d9c8794d75a3e6cb16ae
yuanhy000 <44021993+yuanhy000@users.noreply.github.com>
2020-01-10 17:10:21 +0800

    Update private.php

--- config/private.php
+++ config/private.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'app_id' => '1109912295',
-    'app_secret' => 'Buj8271i1i0sqK9J',
+    'app_id' => '',
+    'app_secret' => '',
     'login_url' => "https://api.q.qq.com/sns/jscode2session?" . "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
 ];
